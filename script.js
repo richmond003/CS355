69b1d94558bb1f6bce0d9bdbfdf47d5938c9d6bd
Richmond <richmondbhnyantakyi@gmail.com>
2025-02-09 14:33:19 -0500

    Initail commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const $ = document.querySelector.bind(document);
+let entries = {}
+
+const toggleBtn = $('toggle');
+
+toggleBtn.addEventListener('click', (e)=>{
+    if(toggleBtn.innerText == 'LIGHT'){
+        toggleBtn.innerText = 'DARK';
+        document.documentElement.setAttribute('theme', 'light')
+        toggleBtn.style.backgroundColor = "black";
+        toggleBtn.style.color = "white";
+    }else{
+        toggleBtn.innerText = 'LIGHT';
+        document.documentElement.removeAttribute('theme');
+        toggleBtn.style.backgroundColor = "white";
+        toggleBtn.style.color = "black";
+    }
+})
+
+async function getTvShowData(title, premiered){
+    try{
+        let res = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${title}&date=${premiered}`);
+        let data = await res.json();
+        document.getElementById("show-title").innerHTML = data.name;
+        document.getElementById("show-cover").src = data.image.original;
+        document.getElementById("show-summary").innerHTML = data.summary;
+        document.getElementById("show-type").innerHTML = data.type;
+        document.getElementById("show-language").innerHTML = data.language;
+        document.getElementById("date").innerHTML = data.premiered;
+    }catch(err){
+        console.error(err)
+    }
+}
+
+const params = new URLSearchParams(location.search);
+params.forEach((value, key)=>{
+    console.log(`Key: ${key}, Value: ${decodeURIComponent(value)}`);
+    if(key === "show_name"){
+        entries.show_name = value;
+    }else{
+        entries.date = value
+    }
+});
+
+const {show_name, date} = entries;
+
+getTvShowData(show_name, date);
+
